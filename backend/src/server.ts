9024b3d68b2dd221707d7081382ff4a9fb57af68
Hsan25 <hasangoing33@gmail.com>
2025-04-08 21:25:00 +0700

    BE- fix vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,9 @@ import app from "app";
 import "./config/db";
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  // process.env
-  console.log(`app listening on  http://localhost:${PORT}`);
+  if (process.env.NODE_ENV !== "production") {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  } else {
+    console.log(`🚀 Server is running in production mode`);
+  }
 });
